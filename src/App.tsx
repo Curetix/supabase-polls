4ce24076e1bd210983e46317a48b66de33384284
Curetix <me@curetix.eu>
2022-08-12 19:53:11 +0200

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Grid, Text } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Grid } from '@chakra-ui/react';
 import ColorModeSwitcher from './components/ColorModeSwitcher';
+import Home from './routes/Home';
 
 function App() {
   return (
     <Grid minH="100vh" p={3}>
       <ColorModeSwitcher justifySelf="flex-end" />
-      <Text>Hello World!</Text>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Grid>
   );
 }
---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Text } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>Hello World!</Text>
+    </Container>
+  );
+}
