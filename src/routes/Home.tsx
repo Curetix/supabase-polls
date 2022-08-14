c21d7f982b7a68ce4d42ff46edbe2f9ce9e5e7f5
Curetix <me@curetix.eu>
2022-08-14 22:38:32 +0200

    Add content to homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,7 +6,7 @@ import {
   Spinner,
   useToast,
   Heading,
-  Button,
+  Button, Divider, Container,
 } from '@chakra-ui/react';
 import { FaChevronRight } from 'react-icons/all';
 import { Poll as PollType } from '../lib/types';
@@ -48,32 +48,38 @@ export default function Home() {
   }, []);
 
   return (
-    <VStack spacing={10}>
-      { isLoading && <Spinner size="xl" /> }
-      { polls && (
-        <VStack spacing={4}>
-          <Heading fontSize={{ base: '3xl' }}>Latest Polls</Heading>
-          {polls?.map((p) => (
-            <Button
-              colorScheme="blue"
-              variant="outline"
-              rightIcon={<FaChevronRight />}
-              onClick={() => navigate(`/${p.shareable_id}`)}
-            >
-              { p.title }
-            </Button>
-          ))}
-        </VStack>
-      )}
-      {!polls && !isLoading && <Alert status="error">Oops, could not load latest polls.</Alert>}
+    <Container>
+      <VStack spacing={10}>
+        <Heading fontSize={{ base: '3xl' }}>Latest Polls</Heading>
 
-      <Button
-        size="lg"
-        colorScheme="green"
-        onClick={() => navigate('/create')}
-      >
-        Create a Poll
-      </Button>
-    </VStack>
+        { isLoading && <Spinner size="xl" /> }
+        { polls && (
+          <VStack spacing={4}>
+            {polls?.map((p) => (
+              <Button
+                key={p.id}
+                colorScheme="blue"
+                variant="outline"
+                rightIcon={<FaChevronRight />}
+                onClick={() => navigate(`/${p.shareable_id}`)}
+              >
+                { p.title }
+              </Button>
+            ))}
+          </VStack>
+        )}
+        {!polls && !isLoading && <Alert status="error">Oops, could not load latest polls.</Alert>}
+
+        <Divider />
+
+        <Button
+          size="lg"
+          colorScheme="green"
+          onClick={() => navigate('/create')}
+        >
+          Create a Poll
+        </Button>
+      </VStack>
+    </Container>
   );
 }
