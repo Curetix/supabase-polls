07df92fa0235e07ebf662239c655f42bce289f33
Curetix <me@curetix.eu>
2022-08-19 15:33:05 +0200

    Merge Supabase SQL migrations

--- supabase/migrations/4_modify_constraint.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.votes
-DROP CONSTRAINT votes_poll_id_fkey,
-ADD  CONSTRAINT votes_poll_id_fkey FOREIGN KEY (poll_id)
-    REFERENCES public.polls (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE;
